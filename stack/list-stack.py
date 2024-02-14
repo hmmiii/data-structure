30d9239a71bbe67016af7179e615a696aefe38d8
hmmi <hmmi@kakao.com>
2024-02-14 16:24:34 +0900

    파이썬 List로 Stack 구현

---
+++ stack/list-stack.py
@@ -0,0 +1,11 @@
+s = list() # 리스트를 객체를 생성해 스택으로 사용
+
+msg = input("문자열 입력: ")
+for c in msg:
+    s.append(c) # c를 스택에 삽입
+
+print("문자열 출력 : ", end='')
+
+while len(s) > 0: # 스택이 공백이 아니라면
+    print(s.pop(), end='')
+print()
