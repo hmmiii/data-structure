0323df8627681279d256b589c178fa7bbc2c7da3
hmmi <hmmi@kakao.com>
2024-02-14 16:29:51 +0900

    queue 모듈의 LIfoQueue를 이용한 Stack 구현

---
+++ stack/lifoQueue-stack.py
@@ -0,0 +1,12 @@
+import queue # 파이썬 큐 모듈 포함
+
+s = queue.LifoQueue(maxsize=100) # 스택 객체 생성(최대 크기 20), maxsize가 0이면 무한대임.
+
+msg = input("문자열 입력 : ")
+for c in msg:
+    s.put(c) # c를 스택에 삽입
+
+print("문자열 출력 : ", end='')
+while not s.empty(): # 스택이 공백 상태가 아니라면
+    print(s.get(), end='')
+print()
