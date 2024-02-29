2f93e5e88a9f93252fd1214f04df9b4d6aa95415
hmmi <hmmi@kakao.com>
2024-03-01 00:39:08 +0900

    클래스만 둠

--- stack/Arraystack.py
+++ stack/Arraystack.py
@@ -24,18 +24,3 @@ class ArrayStack:
         else: pass
     
     def size(self): return self.top+1
-
-
-s = ArrayStack(100) # 새로운 스택 객체 생성.
-
-# 문자열을 입력받아 msg에 저장하고, msg의 각 문자 c를 순서대로 스택에 삽입
-msg = input("문자열 입력 : ")
-for c in msg:
-    s.push(c)
-    
-print("문자열 출력 : ", end='')
-
-# 스택이 공백이 아니면 상단의 문자를 꺼내서 화면에 출력. 이 과정을 공백 상태가 될 때까지 반복
-while not s.isEmpty():
-    print(s.pop(), end ='')
-print()
