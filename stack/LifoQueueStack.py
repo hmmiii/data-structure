9a195deeac0df550f2aefb01a53779abba256c2a
hmmi <hmmi@kakao.com>
2024-03-02 16:54:50 +0900

    LifoQueue 마지막 요소

--- stack/LifoQueueStack.py
+++ stack/LifoQueueStack.py
@@ -6,6 +6,10 @@ msg = input("문자열 입력 : ")
 for c in msg:
     s.put(c) # c를 스택에 삽입
 
+print("사이즈 : ", s.qsize())
+
+print("마지막 요소 : ",s.queue[-1])
+
 print("문자열 출력 : ", end='')
 while not s.empty(): # 스택이 공백 상태가 아니라면
     print(s.get(), end='')
