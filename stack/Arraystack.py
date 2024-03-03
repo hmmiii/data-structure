7cb6fc0f2d48cd5a92f234f6cba3d7abb09dd62f
hmmi <hmmi@kakao.com>
2024-03-03 11:56:09 +0900

    display() 연산 추가

--- stack/Arraystack.py
+++ stack/Arraystack.py
@@ -4,26 +4,33 @@ class ArrayStack:
         self.array = [None] * capacity # 요소 배열
         self.top = -1
     
-    def isEmpty(self) : return self.top == -1
-    def isFull(self) : return self.top == self.capacity-1
+    def isEmpty(self) -> bool : return self.top == -1
+    def isFull(self) -> bool : return self.top == self.capacity-1
 
-    def push(self,item):
+    def push(self,item) -> None:
         if not self.isFull():
             self.top += 1
             self.array[self.top] = item
         else : pass
 
-    def pop(self):
+    def pop(self) -> any:
         if not self.isEmpty():
             self.top -= 1
             return self.array[self.top+1]
         else: pass
 
-    def peek(self):
+    def peek(self) -> any:
         if not self.isEmpty(): return self.array[self.top]
         else: pass
     
-    def size(self): return self.top+1
+    def size(self) -> int : return self.top+1
 
-    def clear(self):
+    def clear(self) -> None:
         self.top = -1
+
+    def display(self) -> None:
+        print("[ ", end='')
+        for i in range(self.top+1):
+            print(" %s " % self.array[i], end='')
+        print(" ]", end='')
+        print()
